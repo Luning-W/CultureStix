cf595ded7dd1eb0df136c2cd8943ad202a1f3b44
Michelle Wu <144174422+wumichelle@users.noreply.github.com>
2024-06-08 22:59:04 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 
-const images = [
-    "Yin-Yang.PNG", "Yin-Yang (Flaming).PNG", "Koi Fish (Navy).PNG", "Koi Fish (Red).PNG",
-    "Moon.PNG", "Moon Bunny.PNG", "Yuanbao (1).PNG", "Yuanbao (2).PNG",
-    "Lantern.PNG", "Lucky Cat.PNG", "Gourd.PNG", "Fan.PNG",
-    "Baozi.PNG", "Peach.PNG", "White Rabbit Candy.PNG", "Soy Sauce.PNG",
-    "Teapot (Curly Handle).PNG", "Teapot (Thick Handle).PNG", "Teapot (Thin Handle).PNG", "Tea.PNG",
-    "Fu (Red).PNG", "Fu (Gold).PNG", "Tian (Blue).PNG", "Tian (Gold).PNG",
-    "Xin (Red).PNG", "Xin (Gold).PNG", "You (Blue).PNG", "You (Gold).PNG",
-    "Gold Frame.PNG", "Thin Gold Border.png"
-];
-
 addOnUISdk.ready.then(() => {
     console.log("addOnUISdk is ready for use.");
+
+    const images = [
+        "Yin-Yang.PNG", "Yin-Yang (Flaming).PNG", "Koi Fish (Navy).PNG", "Koi Fish (Red).PNG",
+        "Moon.PNG", "Moon Bunny.PNG", "Yuanbao (1).PNG", "Yuanbao (2).PNG",
+        "Lantern.PNG", "Lucky Cat.PNG", "Gourd.PNG", "Fan.PNG",
+        "Baozi.PNG", "Peach.PNG", "White Rabbit Candy.PNG", "Soy Sauce.PNG",
+        "Teapot (Curly Handle).PNG", "Teapot (Thick Handle).PNG", "Teapot (Thin Handle).PNG", "Tea.PNG",
+        "Fu (Red).PNG", "Fu (Gold).PNG", "Tian (Blue).PNG", "Tian (Gold).PNG",
+        "Xin (Red).PNG", "Xin (Gold).PNG", "You (Blue).PNG", "You (Gold).PNG",
+        "Gold Frame.PNG", "Thin Gold Border.png"
+    ];
 
     images.forEach((image, index) => {
         document.getElementById(`button${index}`).addEventListener("click", async function() {
